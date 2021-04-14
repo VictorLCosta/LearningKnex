978cf9f35d4bb0819a464350657582e2a67a39a0
VictorLCosta <victorlc2019@outlook.com>
2021-04-14 16:44:49 -0300

    Basic commands

--- index.js
+++ index.js
@@ -1,1 +1,26 @@
 var database = require('./database');
+
+//Insert
+async function insert(){
+    try{
+        var query = await database.insert({name: "1984", author: "George Orwell", theme: "Dystopia"}).into('books');
+        console.log(query.toQuery());
+    }
+    catch(err){
+        console.log(err);
+    }
+}
+
+//Select
+database.select("*").table("users").then(data => {
+    console.log(data);
+}).catch(err => {
+    console.log(err);
+});
+
+//Delete
+database.where({id: 2}).delete().table("books");
+
+//Update
+var query = database.where({id: 5}).update({author: "Tolkien"}).table("books");
+console.log(query.toQuery());
